fecd60c13ac92dfa35e3de0b9856176fc189bca0
Yukie Kato <yukie.kato.28@gmail.com>
2018-09-22 14:36:43 +0900

    Add database.py and __init__.py
    Module to communicate with a database. Implemented connect() and close().
    
    (This commit resolves #1)

---
+++ lib/database.py
@@ -0,0 +1,21 @@
+""" Database.py is a moule to comunicate with a database. """
+
+import sqlite3
+
+__all__ = ['Database']
+
+class Database:
+    """ Database class initializes and manipulates SQLite3 database. """
+    def __init__(self):
+        self.con = None
+        self.cur = None
+
+    def connect_db(self, db_name):
+        """ Connect to the database """
+        self.con = sqlite3.connect(db_name)
+        self.cur = self.con.cursor()
+
+    def close_db(self):
+        """ Close the database """
+        if self.con:
+            self.con.close()
