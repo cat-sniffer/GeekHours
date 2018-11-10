5032346eb9fdd9230e3c8a7f20b1c1c89b0037a3
Yukie Kato <yukie.kato.28@gmail.com>
2018-11-10 13:36:40 +0900

    Add test_command.py
    Unit test for Command class.

---
+++ test/test_command.py
@@ -0,0 +1,105 @@
+""" Unit test for Command module. """
+
+import unittest
+from os import remove, rmdir
+from tempfile import mkstemp, mkdtemp
+from command import Command
+
+
+class TestCommand(unittest.TestCase):
+    """ Test cases of the unit test for Command class """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Prepare the test fixtures
+
+        Create temporary database and test directory first.
+        """
+        cls._db_path = mkdtemp()
+        cls._db_name = mkstemp(suffix='.db', dir=cls._db_path)
+        cls._db_name = str(cls._db_name[1])
+        cls._command = Command(cls._db_name)
+        cls._course_name_python = 'python'
+        cls._course_name_math = 'math'
+        cls._course_name_eng = 'english'
+        cls._courses = [cls._course_name_python, cls._course_name_math, cls._course_name_eng]
+        cls._date = '20181101'
+        cls._duration = '5'
+        cls._donelist = [cls._date, cls._course_name_python, cls._duration]
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Clean-up the fixtures
+
+        Remove database and directory after all the tests have run.
+        """
+        remove(cls._db_name)
+        rmdir(cls._db_path)
+
+    def setUp(self):
+        """ Prepare the fields
+
+        Put values into the fields before calling each test function.
+        """
+        self._command.get_insert_course(self._courses)
+        self._command.get_insert_donelist(self._date, self._course_name_python, self._duration)
+
+    def tearDown(self):
+        """ Remove fields
+
+        Remove values of the fields after each test function is run.
+        """
+        self._command.get_remove_donelist(self._date, self._course_name_python)
+        self._command.get_remove_course(self._course_name_python)
+        self._command.get_remove_course(self._course_name_math)
+        self._command.get_remove_course(self._course_name_eng)
+
+    def test_get_show(self):
+        """ Test for get_show()
+
+        Assert get_show() calls the Database.show().
+        """
+        self.assertIsNone(self._command.get_show('course'))
+        self.assertIsNone(self._command.get_show('donelist'))
+
+    def test_get_isert_course(self):
+        """ Test for get_insert_course()
+
+        Assert get_insert_course() calls the Database.insert_course().
+        """
+        self._command.get_remove_course(self._course_name_python)
+        self._command.get_remove_course(self._course_name_math)
+        self._command.get_remove_course(self._course_name_eng)
+        self.assertIsNone(self._command.get_insert_course(self._courses))
+
+    def test_get_insert_donelist(self):
+        """ Test for get_insert_donelist()
+
+        Assert get_insert_donelist() calls the Database.insert_donelist().
+        """
+        self._command.get_remove_donelist(self._date, self._course_name_python)
+        self.assertIsNone(
+            self._command.get_insert_donelist(self._date, self._course_name_python,
+                                              self._duration))
+
+    def test_get_remove_course(self):
+        """ Test for get_insert_course()
+
+        Assert get_insert_course() calls the Database.remove_course().
+        """
+        self.assertIsNone(self._command.get_remove_course(self._course_name_python))
+        self.assertIsNone(self._command.get_remove_course(self._course_name_math))
+        self.assertIsNone(self._command.get_remove_course(self._course_name_eng))
+
+        # Insertion for tearDown()
+        self._command.get_insert_course(self._courses)
+
+    def test_get_remove_donelist(self):
+        """ Test for get_remove_donelist()
+
+        Assert get_remove_donelist() calls the Database.remove_donelist().
+        """
+        self.assertIsNone(self._command.get_remove_donelist(self._date, self._course_name_python))
+
+        # Insertion for tearDown()
+        self._command.get_insert_donelist(self._date, self._course_name_python, self._duration)
