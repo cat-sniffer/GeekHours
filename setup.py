97a70247015a838e5b0e580d5690e1188d9d052d
Yukie Kato <yukie.kato.28@gmail.com>
2019-04-01 13:35:02 +0900

    Add supported Python versions to the classifiers

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Environment :: Console',
         'Operating System :: POSIX :: Linux',
     ],
