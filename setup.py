d22faaa295876ef2dd9700839c861bd80c30ea40
Yukie Kato <yukie.kato.28@gmail.com>
2019-04-01 12:25:42 +0900

    Delete unused files from setup.py

--- setup.py
+++ setup.py
@@ -19,12 +19,6 @@ setup(
     license='MIT',
     packages=find_packages(),
     scripts=['bin/geekhours'],
-    package_data={'': [
-        'Makefile',
-        '*.yml'
-        '.gitignore',
-        '.style.yapf',
-    ]},
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
